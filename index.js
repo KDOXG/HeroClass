123c64d1dccc679817960bd4d51664d6d8f1bd56
KDOXG <kevin.20pereira@gmail.com>
2024-05-08 04:09:24 -0300

    Challenge 2 submission

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-function main() {
-	let nomeHeroi = "Hero"
-	let xpHeroi = 0
-	
-	let classes = ["Ferro", "Bronze", "Prata", "Prata", "Prata", "Ouro", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
-	let classe = classes[0]
-	for (let it = 0; it < xpHeroi && it <= 10000; it += 1000) {
-		classe = classes.shift()
-	}
-	console.log("O Herói de nome %s está no nível de %s", nomeHeroi, classe)
+function saldoRankeadas(vitorias, derrotas) {
+	return vitorias - derrotas;
 }
 
-main()
+let nomeHeroi = "Hero"
+let nivel = 0
+let vitorias = 20
+let derrotas = 0
+
+let saldo = saldoRankeadas(vitorias, derrotas)
+
+let classes = ["Ferro", "Bronze", "Prata", "Prata", "Prata", "Ouro", "Ouro", "Ouro", "Diamante", "Lendario", "Imortal"]
+let classe = classes[0]
+for (let it = 0; it < saldo && it <= 100; it += 10) {
+	classe = classes.shift()
+}
+
+console.log("O Herói tem de saldo de %d está no nível de %s", saldo, classe)
